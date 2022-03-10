f3d898f5509ee29cfa87ea669f869558d83a927d
michellecabrales <81108550+michellecabrales@users.noreply.github.com>
2022-03-10 11:40:40 -0600

    display test image to main page

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import NavBar from './components/navbar';
 import './App.css'
 import { BrowserRouter as Router } from 'react-router-dom'
+import background from './images/drive.png'
+
 function App() {
   return (
     <Router>
         <NavBar />
     </Router>
+    <img src={background} width="100%" style={{opacity: 0.5}}/> 
   );
 }
 export default App;
