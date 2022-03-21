0b87a4a0075b52e1931ff52e97030c0aa2d430ce
TheTopNep <52087680+TheTopNep@users.noreply.github.com>
2022-03-21 14:25:13 -0500

    Input of Box
    Input box for quick search and future uses.

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,10 +1,24 @@
 import React from 'react'
 import background from '../../images/drive.png'
+import Box from '../../images/purple.png';
+
 export default function Home() {
+
     return (
         <div>
             <h1>Home</h1>
             <img src={background} width="100%" styles={{opacity: 0.5}} />
+            <img src={Box} width = "45%" length = "200%"
+            style={{
+                position: 'absolute ',
+                left: 0,
+                right: 0, 
+                top: 200,
+                bottom: 900
+
+            }}/>
         </div>
+
+        
     )
 }
