6d2d54487bc9ebe11fdc3a4a485323c413ac158f
TheTopNep <52087680+TheTopNep@users.noreply.github.com>
2022-04-14 03:34:02 -0500

    Signup
    Signup has redirects to the Login and will bring user to the dashboard upon clicking signup.

--- src/components/pages/Register/Signup.js
+++ src/components/pages/Register/Signup.js
@@ -1,41 +1,86 @@
-import React , {useState} from 'react';
-import '../../../index.css'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import './Signup.css';
+import {
+  Container,
+  Button,
+  Row,
+  Col,
+  Form,
+  FormControl
+} from "react-bootstrap";
 import './Signup.css'
 
-function SignupPage({Signup, error}) {
-
-    const [details, setDetails] = useState({username: "", email: "", password: ""});
-    
-    const submitHandler = e =>{
-        e.preventDefault();
-
-        Signup(details);
+class Signup extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        email: "",
+        password: ""
+      };
     }
-        
-    return (
-        <form onsubmit={submitHandler}>
-            <div className = "form-inner">
-                <h2>Sign Up!</h2>
-                {(error !== "") ? (<div className = "error">{error}</div>) : ""}
-                <div className="credientials-group">
-                    <label htmlFor="username">Username:</label>
-                    <input type= "text" name = "username" id="username" onChange={e=> setDetails({...details, username: e.target.value})} value={details.username}/>
-                </div>
-                <div className="credientials-group">
-                    <label htmlFor="email">Email:</label>
-                    <input type="email" name="email" id="email" onChange={e=> setDetails({...details, email: e.target.value})} value={details.email} />
-                </div>
-                <div className = "credientials-group">
-                    <label htmlFor="password">Password:</label>
-                    <input type = "password" name="password" id="password" onChange={e=> setDetails({...details, password: e.target.value})} value={details.password}/>
-                </div>
-                <button type = "submit" className ="btn" style = {{height: '30px', width : '85px'}} >Sign Up!</button>
-                
-            </div>
-        </form>
-    )
+    onChange = e => {
+      this.setState({ [e.target.name]: e.target.value });
+    };
+    onSignupClick = () => {
+        const userData = {
+          email: this.state.email,
+          password: this.state.password
+        };
+        console.log("Sign up " + userData.email + " " + userData.password);
+      };
+      render() {
+        return (
+        <Container>
+        <Row>
+          <Col md="4">
+            <h1>Sign up</h1>
+            <Form>
+            <div className="credientials-group">  
+              <Form.Group controlId="email">
+                <Form.Label>Email: </Form.Label>
+                <Form.Control
+                  type="text"
+                  name="email"
+                  placeholder="Enter Email"
+                  value={this.state.email}
+                  onChange={this.onChange}
+                />
+                <FormControl.Feedback type="invalid"></FormControl.Feedback>
+              </Form.Group>
+              </div>
+             <div className="credientials-group">  
+              <Form.Group controlId="passwordId">
+                <Form.Label>Password: </Form.Label>
+                <Form.Control
+                  type="password"
+                  name="password"
+                  placeholder="Enter password"
+                  value={this.password}
+                  onChange={this.onChange}
+                />
+                <Form.Control.Feedback type="invalid"></Form.Control.Feedback>
+              </Form.Group>
+              </div>
+            </Form>
+            <Link to="/Dashboard">
+            <Button 
+              onClick={this.onSignupClick}  
+              type = "submit" className ="btn"
+              style = {{height: '30px', width : '85px'}}
+            >Sign up</Button>
+            </Link>
+            <p className="mt-2">
+              Already have account? <Link to="/login">Login</Link>
+            </p>
+          </Col>
+        </Row>
+      </Container>
+        );
+  }
 }
 
 
-export default SignupPage; 
+
+export default Signup; 
 
