c99fb2a6f3b742dce012324d910d6d8280133dec
TheTopNep <52087680+TheTopNep@users.noreply.github.com>
2022-04-14 03:34:15 -0500

    Navbar
    Navbar changed to include dashboard

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -6,6 +6,8 @@ import Home from '../pages/Home'
 import Login from '../pages/Login/Login'
 import Signup from '../pages/Register/Signup'
 import About from '../pages/About/About';
+import Dashboard from "../../Dashboard";
+
 const NavBar = () => {
     return (
         <>
@@ -26,6 +28,7 @@ const NavBar = () => {
                     <Route path="/About" element={<About />}/>
                     <Route path="/Login" element={<Login />}/>
                     <Route path="Sign-up" element={<Signup />}/>
+                    <Route path="Dashboard" element={<Dashboard />}/>
                 </Routes>
             </Router>
         </>
