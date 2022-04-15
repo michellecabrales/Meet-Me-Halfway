bbc4dab59fbba700bfa0e9c7c484acc83b7279f6
Jmathews95 <98501226+Jmathews95@users.noreply.github.com>
2022-04-15 10:08:44 -0500

    Update firebase.js
    - Added processes for easier switch between development db and production db
    - Added Authorization import

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,25 @@
 //Used https://www.section.io/engineering-education/registration-form-react.js-firebase/
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import 'firebase/auth'
 import { getDatabase } from "firebase/database"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB5Izop7mxgSPW71u5NIOlso_GO8-X5ado",
-  authDomain: "registration-form-5914e.firebaseapp.com",
-  databaseURL: "https://registration-form-5914e-default-rtdb.firebaseio.com",
-  projectId: "registration-form-5914e",
-  storageBucket: "registration-form-5914e.appspot.com",
-  messagingSenderId: "864851409278",
-  appId: "1:864851409278:web:833ceedeeb224db1c59b2f"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const auth = app.auth()
 export const database = getDatabase(app);
+
+
