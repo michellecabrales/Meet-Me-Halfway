8abad82f30808d43fa79add6b737ea146cb02177
joeyrag <85812622+joeyrag@users.noreply.github.com>
2022-04-16 22:15:31 -0500

    Update Login.js
    Uploaded to work with google login

--- src/components/pages/Login/Login.js
+++ src/components/pages/Login/Login.js
@@ -1,60 +1,19 @@
-/*import React from 'react'
+//import '../../../App';
+//import "./LoginIndex.css"
+import {signInWithGoogle} from '../../../firebase'
+import { logOut } from '../../../firebase';
+import { auth } from '../../../firebase';
+//import "bootstrap/dist/css/bootstrap.min.css"
 
-export default function Login() {
-    return (
-        <div style = {{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-            <h1>Login</h1>
-        </div>
-    )
+function Login(){
+  return (
+    
+    <div>
+    <button onClick={signInWithGoogle} > Sign In With Google</button>
+    <h1>{localStorage.getItem("name")}</h1>
+    <h1>{localStorage.getItem("email")}</h1>
+    
+    </div>
+  );
 }
-*/
-/*https://www.youtube.com/watch?v=91qEdc6dSUs&ab_channel=TylerPotts*/
-import React, {useState} from 'react';
-import LoginForm from './LoginForm';
-import './LoginIndex.css'
-function LoginThing(){
-  const adminUser = {
-    email: "test@gmail.com",
-    password: "1234"
-  }
-
-  const [user, setUser] = useState ({name: "", email: ""});
-  const [error, setError] = useState("");
-
-  const Login = details => {
-    console.log(details);
-
-    if (details.email == adminUser.email && details.password == adminUser.password){
-      console.log("Logged in");
-      setUser({
-        name: details.name,
-        email: details.email
-      });
-  }
-    else {
-      console.log("Details do not match");
-      setError("Login Failed")
-    }
-  }
-
-    const Logout = () => {
-      setUser({name: "", email: ""});
-    }
-
-    return (
-      <div className = "LoginThing">
-        {(user.email !="") ? (
-            <div className="welcome">
-              <h2>Welcome, <span>{user.name}</span></h2>
-              <button onClick={Logout}>Logout</button>
-              </div>
-        ) : (
-          <LoginForm Login={Login} error={error}/>
-        )}
-      </div>
-    )
-
-
-}
-
-export default LoginThing;
+export default Login
